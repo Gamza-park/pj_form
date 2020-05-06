b91aea16cdec563703cb442e10ba1658a220502d
Park <youuy28@gmail.com>
2020-05-06 17:50:47 +0900

    form

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "opencv4/opencv2/opencv.hpp"
+
+using namespace cv;
+using namespace std;
+
+
+int main()
+{
+    cout << "Hello OpenCV" << CV_VERSION << endl;
+
+    return 0;
+}
